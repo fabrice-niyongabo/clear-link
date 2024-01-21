2cdcbdfb74a83eb315d227a35ce924c8d71b33d6
Fabrice Niyongabo <ninga.fab@gmail.com>
2024-01-21 17:38:07 +0200

    companies section

---
+++ src/views/Home/Companies/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <v-container>
+      <p class="text-center text-clear-link-gray-text">
+        Join 1,500+ companies already video conferencing the ClearLink way
+      </p>
+      <div class="flex items-center justify-between gap-4 mt-5">
+        <img class="h-[25px]" src="@/assets/img/companies/shopify.png" alt="" />
+
+        <img
+          class="h-[25px]"
+          src="@/assets/img/companies/coinbase.png"
+          alt=""
+        />
+
+        <img class="h-[25px]" src="@/assets/img/companies/dropbox.png" alt="" />
+
+        <img
+          class="h-[25px]"
+          src="@/assets/img/companies/intercom.png"
+          alt=""
+        />
+
+        <img class="h-[25px]" src="@/assets/img/companies/marvel.png" alt="" />
+
+        <img
+          class="h-[20px] w-[150px]"
+          src="@/assets/img/companies/automatic.png"
+          alt=""
+        />
+      </div>
+    </v-container>
+  </section>
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,9 @@
 <template>
   <HeroSection />
+  <Companies />
 </template>
 
 <script setup>
 import HeroSection from "./Hero/index.vue";
+import Companies from "./Companies/index.vue";
 </script>
